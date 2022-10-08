6097aa6756539e00238a1468fed923cd08fbd5ac
carmenzazueta <110991084+carmenzazueta@users.noreply.github.com>
2022-10-08 10:23:14 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showIcon(IconNames.Target)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showIcon(IconNames.SmallSquare)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    basic.showIcon(IconNames.Square)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.StickFigure)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showIcon(IconNames.Duck)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showIcon(IconNames.Chessboard)
+})
